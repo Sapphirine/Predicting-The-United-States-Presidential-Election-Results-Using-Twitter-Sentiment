fbcd045bdf43fd47352602ed78ca412c7cbef519
kirillal <kirill.alshewski@gmail.com>
2015-12-24 00:03:07 -0500

    Create auth.py

---
+++ src/auth.py
@@ -0,0 +1,9 @@
+# Set up Twitter OAuth
+class TwitterOAuth:
+	# Create an app and generate keys
+	consumer_key=""
+	consumer_secret=""
+
+	# Generate tokens
+	access_token=""
+	access_token_secret=""
